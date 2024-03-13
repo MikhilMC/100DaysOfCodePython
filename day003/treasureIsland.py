5c280ab795c500f624e66616e1ae86910f7b5f6b
MikhilMC <mikhilmohan13@gmail.com>
2024-03-13 23:15:09 +0530

    Day 3 - Made version 2 of BMI calculator, leap year finder, love calculator, pizza order, roller coster, and a treasure island using python

--- day003/treasureIsland.py
+++ day003/treasureIsland.py
@@ -25,16 +25,16 @@ print("Your mission is to find the treasure.")
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
-left_or_right = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right" ')
-if left_or_right.lower() == "left":
-    swim_or_wait = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. ')
-    if swim_or_wait.lower() == "wait":
-        which_door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ")
-        if which_door.lower() == "red":
+left_or_right = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right" ').lower()
+if left_or_right == "left":
+    swim_or_wait = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. ').lower()
+    if swim_or_wait == "wait":
+        which_door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ").lower()
+        if which_door == "red":
             print("It\'s a room full of fire. Game Over.")
-        elif which_door.lower() == "yellow":
+        elif which_door == "yellow":
             print("You found the treasure! You Win!")
-        elif which_door.lower() == "blue":
+        elif which_door == "blue":
             print("You enter a room of beasts. Game Over.")
         else:
             print("You chose a door that doesn\'t exist. Game Over.")
