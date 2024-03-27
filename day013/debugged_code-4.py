58d642530eeeebd0a0416185efecb97041e06f83
MikhilMC <mikhilmohan13@gmail.com>
2024-03-27 20:00:38 +0530

    Day 13 - debugging

---
+++ day013/debugged_code-4.py
@@ -0,0 +1,6 @@
+############DEBUGGING#####################
+
+# # Fix the Errors
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"You can drive at age {age}.")
---
+++ day013/debugged_code-5.py
@@ -0,0 +1,14 @@
+############DEBUGGING#####################
+
+# #Print is Your Friend
+pages = 0
+word_per_page = 0
+print(pages, word_per_page)
+
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+print(pages, word_per_page)
+
+total_words = pages * word_per_page
+print(total_words)
+
